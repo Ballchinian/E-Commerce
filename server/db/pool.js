7fb6019fa7ec08d28ce00e51642e6f554f6bc733
Ethan <ethantnewiss@hotmail.co.uk>
2025-08-11 00:00:02 +0100

    check check for if dataBASE URL EXISTS

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 //Private details are redirected to be able to access SQL database
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,

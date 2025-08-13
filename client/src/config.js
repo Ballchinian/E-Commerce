e78941d590112ec0e7a2746ef652e7c9fb107a51
Ethan <ethantnewiss@hotmail.co.uk>
2025-08-13 23:55:58 +0100

    PLEASASASE

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = "https://e-commerce-production-259b.up.railway.app" || 'http://localhost:4000';
+const API_BASE_URL = "https://e-commerce-production-259b.up.railway.app" || 'http://localhost:4000';
+
+module.exports = { API_BASE_URL };

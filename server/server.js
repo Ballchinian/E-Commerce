00ef21dae8ec6c106ed8f2db526038858f0c422e
Ethan <ethantnewiss@hotmail.co.uk>
2025-08-13 16:32:23 +0100

    changed the origin to be more speicifc

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Enable CORS for all routes first
 app.use(cors({
-  origin: 'https://e-commercelive.netlify.app',
+  origin: 'https://e-commercelive.netlify.app/addProduct',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));

4571b2aad4ec0fedfa7c21c836757438c27347b6
Ethan <ethantnewiss@hotmail.co.uk>
2025-08-13 23:25:00 +0100

    ignore verifyToken and api routes to see if it works

--- server/server.js
+++ server/server.js
@@ -15,11 +15,11 @@ app.use(express.static('public'));
 
 // Public routes
 app.use('/auth', require('./routes/authRoutes'));
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+//app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 // Protected routes
-app.use(verifyToken);
-app.use('/api', require('./routes/apiRoutes'));
+//app.use(verifyToken);
+//app.use('/api', require('./routes/apiRoutes'));
 app.use('/product', require('./routes/productRoutes'));
 app.use('/cart', require('./routes/cartRoutes'));
 app.use('/order', require('./routes/orderRoutes'));
